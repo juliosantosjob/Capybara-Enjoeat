84e0b12c5cbc67cce93ff9db51e7954c49723668
juliosantossjob <114118690+juliosantossjob@users.noreply.github.com>
2022-09-22 13:58:01 -0300

    Create hooks.rb

---
+++ features/support/hooks.rb
@@ -0,0 +1,14 @@
+Before do
+  @rest = Rest.new
+  page.current_window.resize_to(1920, 1080)
+end
+
+After do
+  shot = page.save_screenshot("logs/temp_evidence.png")
+
+  Allure.add_attachment(
+    name: "Screenshot",
+    type: Allure::ContentType::PNG,
+    source: File.open(shot),
+  )
+end
