89a3415b3a2bb163005f35b3641ab631e265939d
Julio Santos <julioc.9730@gmail.com>
2022-10-22 16:10:26 -0300

    Update hooks.rb

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -3,12 +3,12 @@ Before do
   page.current_window.resize_to(1920, 1080)
 end
 
-After do
-  shot = page.save_screenshot("logs/temp_evidence.png")
+AfterStep do
+  shot = page.save_screenshot('logs/temp_evidence.png')
 
   Allure.add_attachment(
-    name: "Screenshot",
+    name: 'Screenshot',
     type: Allure::ContentType::PNG,
-    source: File.open(shot),
+    source: File.open(shot)
   )
 end
